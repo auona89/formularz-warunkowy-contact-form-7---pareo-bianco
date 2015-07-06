c75c64d1dd01dc5869f8e37101dfc49a86afbafc
Iwona Kober <bszkontakt@gmail.com>
2015-07-06 07:13:39 +0200

    ....
    Dodaj ścieżki do headera

---
+++ dodaj-to-do-header.php
@@ -0,0 +1,6 @@
+
+
+<!-- Dodaj to do headera-->
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/1.7.1/jquery-1.7.1.min.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/hidefieldsScript.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/select.js"></script>
